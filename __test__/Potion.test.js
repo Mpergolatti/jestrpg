509b93218209aae9d5988016e56a22f454045737
Mpergolatti <Mpergolatti@gmail.com>
2022-10-09 18:14:06 -0400

    changed some code at the end of 10.1

--- __test__/Potion.test.js
+++ __test__/Potion.test.js
@@ -1,12 +1,12 @@
-// const Potion = require('../lib/Potion.js');
+const Potion = require('../lib/Potion.js');
 
-// test('creates a health potion object', ()=> {
-//   const potion = new Potion('health');
+test('creates a health potion object', ()=> {
+  const potion = new Potion('health');
 
-//   expect(potion.name).toBe('health');
-//   expect(potion.value).toEqual(expect.any(Number));
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
 
-// });
+});
 
 test('creates a random potion object', ()=> {
   const potion = new Potion();
@@ -17,15 +17,15 @@ test('creates a random potion object', ()=> {
 
 });
 
-function Potion(name) {
-  this.types = ['strength', 'agility', 'health'];
-  this.name = name || this.types[Math.floor(Math.random() * this.types.length)];
+// function Potion(name) {
+//   this.types = ['strength', 'agility', 'health'];
+//   this.name = name || this.types[Math.floor(Math.random() * this.types.length)];
 
-  if (this.name === 'health') {
-    this.value = Math.floor(Math.random() * 10 + 30);
-  } else {
-    this.value = Math.floor(Math.random() * 5 + 7);
-  }
-}
+//   if (this.name === 'health') {
+//     this.value = Math.floor(Math.random() * 10 + 30);
+//   } else {
+//     this.value = Math.floor(Math.random() * 5 + 7);
+//   }
+// }
 
-module.exports = Potion;
+// module.exports = Potion;
